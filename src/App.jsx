fa666c1a40c69cd4b466dabd97419443fe62d8a0
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2025-08-15 17:11:32 +0430

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
+import Auth from "./Pages/Auth";
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
