0c20e65f8dee05f6b6c8ff3a7db286dd62193964
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2025-08-16 16:55:41 +0430

    Update Start.jsx

--- src/Components/Start.jsx
+++ src/Components/Start.jsx
@@ -1,22 +1,28 @@
 import Title from "./Title";
+import gift from "../assets/gift.png";
 
 export default function Start({ handlePage }) {
   return (
-    <div className="w-full max-w-lg mx-auto p-6 bg-white rounded-2xl shadow-lg mt-8">
-      <Title />
-      <div className="flex flex-col items-center justify-center space-y-4 mt-10">
-        <p className="text-xl">
-          سلام به سایت ما خوش اومدی اینجا میتونی شانستو امتحان کنی تا به جاییزه
-          بزرگ برسی.
-        </p>
-        <p className="text-xl">میخوای شانستو امتحان کنی؟</p>
-        <button
-          onClick={() => handlePage("random")}
-          className="mt-4 px-6 py-2 text-xl w-full bg-blue-600 transition-colors cursor-pointer text-white rounded-lg hover:bg-blue-700"
-        >
-          بزن بریم!
-        </button>
+    <div className="w-full flex flex-col items-center space-y-8 md:space-x-12 md:flex-row max-w-2xl mx-auto p-6 bg-white rounded-2xl shadow-lg mt-8">
+      <div className="">
+        <Title />
+        <div className="flex flex-col items-center justify-center space-y-4 mt-10">
+          <p className="text-xl">
+            سلام به سایت ما خوش اومدی اینجا میتونی شانستو امتحان کنی تا به
+            جاییزه بزرگ برسی.
+          </p>
+          <p className="text-xl">میخوای شانستو امتحان کنی؟</p>
+          <button
+            onClick={() => handlePage("random")}
+            className="mt-4 px-6 py-2 text-xl w-full bg-blue-600 transition-colors cursor-pointer text-white rounded-lg hover:bg-blue-700"
+          >
+            بزن بریم!
+          </button>
+        </div>
       </div>
+        <div className="w-48 md:w-72">
+          <img src={gift} />
+        </div>
     </div>
   );
 }
